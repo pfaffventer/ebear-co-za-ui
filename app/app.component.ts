2b055db6acd2e782a297f07e8bdb73f443f24437
pfaff <johanv@tru-cape.co.za>
2016-09-14 10:14:33 +0200

    Signed-off-by: pfaff <johanv@tru-cape.co.za> added link to bootstrap.min.css in index.html

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  // templateUrl: 'app/app.component.html'
-  template: '<h1>My First Angular 2 App</h1>'
+  templateUrl: 'app/app.component.html'
+  // template: '<h1>My First Angular 2 App</h1>'
 })
 export class AppComponent { }
